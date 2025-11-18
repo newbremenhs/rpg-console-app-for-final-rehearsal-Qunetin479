5ce528c326afd7e7554a98af101d6204c3930fb0
Qunetin479 <27frickq@newbremenschools.org>
2025-11-18 19:13:38 +0000

    fcv

--- Program.cs
+++ Program.cs
@@ -1,34 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+Random dice = new Random();
 
-app.Urls.Add("http://localhost:5000");
+int monsterAC = 18;
 
-app.MapGet("/", () => "Hello World!");
+Console.WriteLine(" Roll your d20!");
+Console.ReadLine();
 
-app.MapGet("/{cityName}/weather", GetWeatherByCity);
+int d20Roll = dice.Next(20);
+Console.WriteLine("You rolled a " + Convert.ToString(d20Roll));
 
-app.Run();
-
-
-Weather GetWeatherByCity(string cityName)
-{
-    app.Logger.LogInformation($"Weather requested for {cityName}.");
-    var weather = new Weather(cityName);
-    return weather;
+if(d20Roll>monsterAC) {
+    Console.WriteLine("You hit the monster!");
+} else{
+    Console.WriteLine("Your atack missed.");
 }
+    
 
-public record Weather
-{
-    public string City { get; set; }
-
-    public Weather(string city)
-    {
-        City = city;
-        Conditions = "Cloudy";
-        // Temperature here is in celsius degrees, hence the 0-40 range.
-        Temperature = new Random().Next(0,40).ToString();
-    }
-
-    public string Conditions { get; set; }
-    public string Temperature { get; set; }
-}
+    
